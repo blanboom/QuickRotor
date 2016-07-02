e3158986cfe2665aa7dc7a1be75925147ae5bc91
Blanboom <i@blanboom.org>
2016-07-02 21:41:26 +0800

    完成遥控器基础程序

--- Arduino_Remote/src/Arduino_Remote.ino
+++ Arduino_Remote/src/Arduino_Remote.ino
@@ -9,41 +9,114 @@
  *     SCK       <--->  SCK
  */
 
+/* 按键：
+ * 控制增减：
+ *     q: 油门 +
+ *     a: 油门 -
+ *     w: yaw +
+ *     s: yaw -
+ *     e: roll +
+ *     d: roll -
+ *     r: pitch +
+ *     f: pitch -
+ * 设置 PID:
+ *     z: Yaw Kp
+ *     x: Yaw Ki
+ *     c: Yaw Kd
+ *     v: Roll Kp
+ *     b: Roll Ki
+ *     n: Roll Kd
+ *     m: Pitch Kp
+ *     ,: Pitch Ki
+ *     .: Pitch Kd
+ *     后面跟上 PID 数值，整数与小数部分各一位，例如 5.7, 以 $ 结束
+ */
+
 #include <Mirf.h>
 #include <MirfHardwareSpiDriver.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 
-void setup() {
-  Serial.begin(115200);
-  Serial.print("Init...");
+uint8_t data[4];
 
-  /* 初始化 nRF24L01 */
-  pinMode(7, OUTPUT);
-  digitalWrite(7, 1);
-  Mirf.cePin = 8;  // 设置 CE 引脚
-  Mirf.csnPin = 7; // 设置 CSN 引脚
-  Mirf.spi = &MirfHardwareSpi;
-  Mirf.init();
-  Mirf.setRADDR((byte *)"63472");
-  Mirf.payload = 8 * sizeof(uint8_t); // 一次发送四字节
-  Mirf.channel = 57; // 发送通道，可以填 0~128，收发必须一致。
-  Mirf.config();
+void setup()
+{
+    Serial.begin(115200);
+    Serial.print("Init...");
 
-  Serial.println("Init OK!");
+    /* 初始化 nRF24L01 */
+    pinMode(7, OUTPUT);
+    digitalWrite(7, 1);
+    Mirf.cePin  = 8; // 设置 CE 引脚
+    Mirf.csnPin = 7; // 设置 CSN 引脚
+    Mirf.spi    = &MirfHardwareSpi;
+    Mirf.init();
+    Mirf.setRADDR((byte *)"63472");
+    Mirf.payload = 4 * sizeof(uint8_t); // 一次发送四字节
+    Mirf.channel = 57; // 发送通道，可以填 0~128，收发必须一致。
+    Mirf.config();
+
+    Serial.println("Init OK!");
 }
 
-void loop() {
-  /* 发送数据 */
-  uint8_t data[] = {0x18, 0x25, 0x36, 0x74, 0x18, 0x25, 0x36, 0x74};
-  Serial.print("Sending");
-  Mirf.setTADDR((byte *)"deytr"); // 设置发送地址
-  Mirf.send(data);
-  while (Mirf.isSending()) {
-      Serial.print('.');
-  }
+void loop()
+{
+    if (Serial.available() > 0) {
+        char tmp = Serial.read();
+        char tmp2;
+        if (tmp == 'q' || tmp == 'a' || tmp == 'w' || tmp == 's' || tmp == 'e' ||
+            tmp == 'd' || tmp == 'r' || tmp == 'f') {
+            data[1] = 0;
+            data[2] = 0;
+            data[3] = 0;
+            switch (tmp) {
+            case 'q': data[0] = 0x01; break;
+            case 'a': data[0] = 0x02; break;
+            case 'w': data[0] = 0x04; break;
+            case 's': data[0] = 0x08; break;
+            case 'e': data[0] = 0x10; break;
+            case 'd': data[0] = 0x20; break;
+            case 'r': data[0] = 0x40; break;
+            case 'f': data[0] = 0x80; break;
+            default: break;
+            }
+            sendData();
+        }
+        if (tmp == 'z' || tmp == 'x' || tmp == 'c' || tmp == 'v' || tmp == 'b' ||
+            tmp == 'n' || tmp == 'm' || tmp == ',' || tmp == '.') {
+            data[0] = 0;
+            while (Serial.available() == 0) ;
+            data[2] = Serial.read(); if (data[2] < '0' || data[2] > '9') break;
+            while (Serial.available() == 0) ;
+            tmp2 = Serial.read(); if (tmp != '.') break;
+            while (Serial.available() == 0) ;
+            data[3] = Serial.read(); if (data[3] < '0' || data[3] > '9') break;
+            while (Serial.available() == 0) ;
+            tmp2 = Serial.read(); if (tmp != '$') break;
+            switch (tmp) {
+            case 'z': data[1] = 'a'; break; // TODO: 发送数据
+            case 'x': data[1] = 'b'; break;
+            case 'c': data[1] = 'c'; break;
+            case 'v': data[1] = 'd'; break;
+            case 'b': data[1] = 'e'; break;
+            case 'n': data[1] = 'f'; break;
+            case 'm': data[1] = 'g'; break;
+            case ',': data[1] = 'h'; break;
+            case '.': data[1] = 'i'; break;
+            default: break;
+            }
+            sendData();
+        }
+    }
+}
 
-  Serial.println("Sent!");
-
-  delay(1000);
+void sendData()
+{
+    Serial.print("Sending");
+    Mirf.setTADDR((byte *)"deytr"); // 设置发送地址
+    Mirf.send(data);
+    while (Mirf.isSending()) {
+        Serial.print('.');
+    }
+    Serial.println("Sent!");
 }
